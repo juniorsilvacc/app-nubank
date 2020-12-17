116bb7b1dadb4593d14be1a6dede2d27207703b9
juniorsilvacc <juniorsilvacc@hotmail.com>
2020-12-16 23:46:40 -0300

    Animated

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,8 @@
+import { Animated } from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
@@ -8,13 +11,69 @@ import Menu from '~/components/Menu';
 import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
 
 export default function Main(){
+
+  let offset = 0;
+
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true },
+  )
+
+  function onHandlerStateChange(event){
+      if(event.nativeEvent.oldState == State.ACTIVE){
+        let opened = false;
+        const { translationY } = event.nativeEvent;
+
+        offset += translationY;
+
+        if (translationY >= 100){
+          opened = true;
+        } else {
+          translateY.setValue(offset);
+          translateY.setOffset(0);
+          offset = 0;
+        }
+
+        Animated.timing(translateY, {
+          toValue: opened ? 380 : 0,
+          duration: 200,
+          useNativeDriver: true,
+        }).start(() => {
+          offset = opened ? 380 : 0;
+          translateY.setOffset(offset);
+          translateY.setValue(0);
+        });
+      }
+  }
+
   return (
     <Container>
       <Header/>
-      
+
       <Content>
-        <Menu />
-        <Card>
+        <Menu translateY={translateY} />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+        <Card style={{
+          transform: [{
+            translateY: translateY.interpolate({
+              inputRange: [-350, 0, 380],
+              outputRange: [-50, 0, 380],
+              extrapolate: 'clamp',
+            }),
+          }],
+        }}>
           <CardHeader>
             <Icon name="attach-money" size={28} color="#666" />
             <Icon name="visibility-off" size={28} color="#666" />
@@ -29,9 +88,11 @@ export default function Main(){
             </Annotation>
           </CardFooter>
         </Card>
+        </PanGestureHandler>
+
       </Content>
 
-      <Tabs/>
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
